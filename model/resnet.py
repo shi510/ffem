d44586a9368e685d42e730cbb517f277f587fbfd
Im Sunghoon <shi510.kor@gmail.com>
2019-07-27 12:52:17 +0900

    correct residual block and typo.

--- model/resnet.py
+++ model/resnet.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow.keras.layers as layers
 
 # 224, 192, 160
-def RestNet18(shape):
+def ResNet18(shape):
     input = tf.keras.Input(shape)
     x = _conv_bn_act(input, 64, 7, 2, 'same', layers.ReLU())
     x = layers.MaxPool2D(pool_size=3, strides=2, padding='same')(x)
@@ -17,19 +17,20 @@ def RestNet18(shape):
     return tf.keras.Model(input, feature)
 
 def _res_block1(input):
-    for n in range(2):
-        shortcut = input
-        x = _conv_bn_act(input, 64, 3, 1, 'same', layers.ReLU())
+    x = input
+    for _ in range(2):
+        shortcut = x
+        x = _conv_bn_act(x, 64, 3, 1, 'same', layers.ReLU())
         x = _conv_bn_act(x, 64, 3, 1, 'same', layers.ReLU())
         x = layers.Add()([x, shortcut])
     return x
 
 def _res_block2(input):
-    shortcut = layers.Conv2D(128, 2, 2, 'valid', use_bias=False)(input)
+    shortcut = _res_downsample(128)(input)
     x = _conv_bn_act(input, 128, 3, 2, 'same', layers.ReLU())
     x = _conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
     x = layers.Add()([x, shortcut])
-    for n in range(2):
+    for _ in range(2):
         shortcut = x
         x = _conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
         x = _conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
@@ -37,16 +38,19 @@ def _res_block2(input):
     return x
 
 def _res_block3(input):
-    shortcut = layers.Conv2D(256, 2, 2, 'valid', use_bias=False)(input)
+    shortcut = _res_downsample(256)(input)
     x = _conv_bn_act(input, 256, 3, 2, 'same', layers.ReLU())
     x = _conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
     x = layers.Add()([x, shortcut])
-    for n in range(2):
+    for _ in range(2):
         shortcut = x
         x = _conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
         x = _conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
         x = layers.Add()([x, shortcut])
     return x
+
+def _res_downsample(filter):
+    return layers.Conv2D(filter, 1, 2, 'valid', use_bias=False)
 
 def _conv_bn_act(input, filter, ksize, stride, padding, act):
     x = layers.Conv2D(
@@ -86,19 +90,20 @@ class ResNet34Experiment(tf.keras.Model):
         return x
 
     def _res_block1(self, input):
-        for n in range(3):
-            shortcut = input
-            x = self._conv_bn_act(input, 32, 3, 1, 'same', layers.ReLU())
+        x = input
+        for _ in range(3):
+            shortcut = x
+            x = self._conv_bn_act(x, 32, 3, 1, 'same', layers.ReLU())
             x = self._conv_bn_act(x, 32, 3, 1, 'same', layers.ReLU())
             x = layers.Add()([x, shortcut])
         return x
 
     def _res_block2(self, input):
-        shortcut = layers.Conv2D(64, 1, 2, 'valid')(input)
+        shortcut = self._res_downsample(64)(input)
         x = self._conv_bn_act(input, 64, 3, 2, 'same', layers.ReLU())
         x = self._conv_bn_act(x, 64, 3, 1, 'same', layers.ReLU())
         x = layers.Add()([x, shortcut])
-        for n in range(3):
+        for _ in range(3):
             shortcut = x
             x = self._conv_bn_act(x, 64, 3, 1, 'same', layers.ReLU())
             x = self._conv_bn_act(x, 64, 3, 1, 'same', layers.ReLU())
@@ -106,11 +111,11 @@ class ResNet34Experiment(tf.keras.Model):
         return x
 
     def _res_block3(self, input):
-        shortcut = layers.Conv2D(128, 1, 2, 'valid')(input)
+        shortcut = self._res_downsample(128)(input)
         x = self._conv_bn_act(input, 128, 3, 2, 'same', layers.ReLU())
         x = self._conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
         x = layers.Add()([x, shortcut])
-        for n in range(5):
+        for _ in range(5):
             shortcut = x
             x = self._conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
             x = self._conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
@@ -118,16 +123,19 @@ class ResNet34Experiment(tf.keras.Model):
         return x
 
     def _res_block4(self, input):
-        shortcut = layers.Conv2D(256, 1, 2, 'valid')(input)
+        shortcut = self._res_downsample(256)(input)
         x = self._conv_bn_act(input, 256, 3, 2, 'same', layers.ReLU())
         x = self._conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
         x = layers.Add()([x, shortcut])
-        for n in range(2):
+        for _ in range(2):
             shortcut = x
             x = self._conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
             x = self._conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
             x = layers.Add()([x, shortcut])
         return x
+
+    def _res_downsample(self, filter):
+        return layers.Conv2D(filter, 1, 2, 'valid', use_bias=False)
 
     def _conv_bn_act(self, input, filter, ksize, stride, padding, act):
         x = layers.Conv2D(
@@ -168,19 +176,20 @@ class ResNet18Experiment(tf.keras.Model):
         return feature
 
     def _res_block1(self, input):
-        for n in range(2):
-            shortcut = input
-            x = self._conv_bn_act(input, 64, 3, 1, 'same', layers.ReLU())
+        x = input
+        for _ in range(2):
+            shortcut = x
+            x = self._conv_bn_act(x, 64, 3, 1, 'same', layers.ReLU())
             x = self._conv_bn_act(x, 64, 3, 1, 'same', layers.ReLU())
             x = layers.Add()([x, shortcut])
         return x
 
     def _res_block2(self, input):
-        shortcut = layers.Conv2D(128, 2, 2, 'valid', use_bias=False)(input)
+        shortcut = self._res_downsample(128)(input)
         x = self._conv_bn_act(input, 128, 3, 2, 'same', layers.ReLU())
         x = self._conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
         x = layers.Add()([x, shortcut])
-        for n in range(2):
+        for _ in range(2):
             shortcut = x
             x = self._conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
             x = self._conv_bn_act(x, 128, 3, 1, 'same', layers.ReLU())
@@ -188,16 +197,19 @@ class ResNet18Experiment(tf.keras.Model):
         return x
 
     def _res_block3(self, input):
-        shortcut = layers.Conv2D(256, 2, 2, 'valid', use_bias=False)(input)
+        shortcut = self._res_downsample(256)(input)
         x = self._conv_bn_act(input, 256, 3, 2, 'same', layers.ReLU())
         x = self._conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
         x = layers.Add()([x, shortcut])
-        for n in range(2):
+        for _ in range(2):
             shortcut = x
             x = self._conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
             x = self._conv_bn_act(x, 256, 3, 1, 'same', layers.ReLU())
             x = layers.Add()([x, shortcut])
         return x
+
+    def _res_downsample(self, filter):
+        return layers.Conv2D(filter, 1, 2, 'valid', use_bias=False)
 
     def _conv_bn_act(self, input, filter, ksize, stride, padding, act):
         x = layers.Conv2D(
