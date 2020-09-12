469c975162d26d08a0b53f095a940666d81c3ff2
Im Sunghoon <shi510.kor@gmail.com>
2020-09-12 17:19:07 +0900

    Add a new method to make tf dataset.

--- example/train/input_pipeline.py
+++ example/train/input_pipeline.py
@@ -8,8 +8,11 @@ TF_AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
 def flip(x: tf.Tensor):
-    x = tf.image.random_flip_left_right(x)
-    return x
+    return tf.image.random_flip_left_right(x)
+
+
+def gray(x):
+        return tf.image.grayscale_to_rgb(tf.image.rgb_to_grayscale(x))
 
 
 def color(x: tf.Tensor):
@@ -19,10 +22,6 @@ def color(x: tf.Tensor):
         x = tf.image.random_brightness(x, 0.05)
         x = tf.image.random_contrast(x, 0.7, 1.3)
         return x
-
-
-    def gray(x):
-        return tf.image.grayscale_to_rgb(tf.image.rgb_to_grayscale(x))
 
 
     choice = tf.random.uniform([], 0, 1, dtype=tf.float32)
@@ -93,3 +92,59 @@ def make_RFW_tfdataset(root_path, race_list, num_id, batch_size, img_shape, oneh
     ds = ds.prefetch(TF_AUTOTUNE)
     return ds, num_class
 
+
+def make_RFW_tfdataset_v2(list_file, root_path, num_id, batch_size, img_shape, onehot=False):
+    pathes, labels, boxes = utils.read_dataset_from_json(list_file)
+    assert len(pathes) == len(labels) and len(pathes) == len(boxes)
+    ds = tf.data.Dataset.from_tensor_slices((pathes, labels, boxes))
+    ds = ds.shuffle(len(pathes))
+
+    def _load_and_preprocess_image(path, label, box):
+        path = root_path + os.sep + path
+        image = tf.io.read_file(path)
+        image = tf.io.decode_jpeg(image, channels=3)
+        # shape = [Height, Width, Channel]
+        shape = tf.shape(image)
+        # shape = [Height, Height, Width, Width]
+        shape = tf.repeat(shape, [2, 2, 0])
+        # shape = [Height, Width, Height, Width]
+        shape = tf.scatter_nd([[0], [2], [1], [3]], shape, tf.constant([4]))
+        # Normalize [y1, x1, y2, x2] box by width and height.
+        box /= tf.cast(shape, tf.float32)
+        # Normalize images to the range [0, 1].
+        image /= 255
+        return image, label, box
+
+
+    def _random_crop(x: tf.Tensor, label, box):
+        def crop_rnd_wrap(x, box):
+            scale = tf.random.uniform([4], -0.2, 0.2)
+            box += box * scale
+            box = tf.clip_by_value(box, 0, 1)
+            return tf.image.crop_and_resize([x], [box], [0], img_shape)[0]
+
+
+        def crop_wrap(x, box):
+            return tf.image.crop_and_resize([x], [box], [0], img_shape)[0]
+
+
+        choice = tf.random.uniform(shape=[], minval=0., maxval=1.)
+        # Only apply cropping 50% of the time
+        cond = tf.cond(choice < 0.5,
+            lambda: crop_wrap(x, box), lambda: crop_rnd_wrap(x, box))
+        return cond, label
+
+
+    ds = ds.map(_load_and_preprocess_image)
+    ds = ds.map(_random_crop)
+    augmentations = [flip, gray]
+    for f in augmentations:
+        choice = tf.random.uniform([], 0, 1)
+        ds = ds.map(lambda x, label: (tf.cond(choice > 0.5, lambda: f(x), lambda: x), label),
+            num_parallel_calls=TF_AUTOTUNE)
+    ds = ds.map(lambda x, label: (tf.clip_by_value(x, 0, 1), label))
+    if onehot:
+        ds = ds.map(lambda img, label : ((img, label), tf.one_hot(label, num_id)))
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(TF_AUTOTUNE)
+    return ds
