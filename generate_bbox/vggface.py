9265eb463fbcec7d179684eb998b9c82201b0d69
Im Sunghoon <shi510.kor@gmail.com>
2020-10-14 18:55:10 +0900

    Fix vggface2 bounding box generator

--- generate_bbox/vggface.py
+++ generate_bbox/vggface.py
@@ -10,9 +10,12 @@ def save_bbox_to_json(lndmk_file, out_file):
     with open(lndmk_file, 'r') as lndmk_f:
         reader = csv.DictReader(lndmk_f, delimiter=',')
         for n, line in enumerate(reader):
-            img_path = line['NAME_ID']
+            img_path = line['NAME_ID'] + '.jpg'
             label = line['NAME_ID'].split('/')[0]
-            bbox = [line['X'], line['Y'], line['W'], line['H']]
+            x1 = int(line['X'])
+            y1 = int(line['Y'])
+            x2 = x1 + int(line['W'])
+            y2 = y1 + int(line['H'])
             if label not in label_book:
                 label_count += 1
                 label_book[label] = label_count
@@ -22,8 +25,8 @@ def save_bbox_to_json(lndmk_file, out_file):
             label = label_book[label]
             results[img_path] = {
                 'label': label,
-                'x1': int(bbox[0]), 'y1': int(bbox[1]),
-                'x2': int(bbox[2]), 'y2': int(bbox[3])
+                'x1': x1, 'y1': y1,
+                'x2': x2, 'y2': y2
             }
             if n % 10000 == 0:
                 print('{} images are processed'.format(n))
