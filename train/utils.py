13756788709a12b69b1c084ee67cfce6f863689a
Im Sunghoon <shi510.kor@gmail.com>
2021-01-22 16:14:51 +0900

    Add pairwise distance

--- train/utils.py
+++ train/utils.py
@@ -11,6 +11,23 @@ import tensorflow as tf
 from tensorboard.plugins import projector
 
 
+@tf.function
+def pairwise_distance(A, B):
+    """
+    (a-b)^2 = a^2 -2ab + b^2
+    A shape = (N, D)
+    B shaep = (C, D)
+    result shape = (N, C)
+    """
+    row_norms_A = tf.math.reduce_sum(tf.square(A), axis=1)
+    row_norms_A = tf.reshape(row_norms_A, [-1, 1])  # Column vector.
+
+    row_norms_B = tf.math.reduce_sum(tf.square(B), axis=1)
+    row_norms_B = tf.reshape(row_norms_B, [1, -1])  # Row vector.
+
+    return row_norms_A - 2 * tf.matmul(A, tf.transpose(B)) + row_norms_B
+
+
 def open_config_file(file_path):
     with open(file_path, 'r') as f:
         file = json.load(f)
