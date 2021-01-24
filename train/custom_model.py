de1dc49da46326ecf8f5a0967becd766b7c2a75a
Im Sunghoon <shi510.kor@gmail.com>
2021-01-24 17:25:19 +0900

    add proxynca model

--- train/custom_model.py
+++ train/custom_model.py
@@ -1,3 +1,4 @@
+from train.loss.proxynca import ProxyNCALoss
 from train.loss.softmax import SoftmaxLoss
 from train.loss.center_loss import CenterLoss
 from train.loss.addictive_margin_loss import AddictiveMarginLoss
@@ -51,6 +52,51 @@ class SoftmaxModel(tf.keras.Model):
     def metrics(self):
         return [self.loss_tracker, self.loss_tracker_softmax, self.loss_tracker_center]
 
+
+class ProxyModel(tf.keras.Model):
+
+    def __init__(self, n_embeddings, n_classes, proxy_weight=1e-3, proxy_lr=1e-3, scale=30, **kwargs):
+        super(ProxyModel, self).__init__(**kwargs)
+        self.n_embeddings = n_embeddings
+        self.n_classes = n_classes
+        self.proxy_lr = proxy_lr
+        self.softmax_loss = SoftmaxLoss(n_embeddings, n_classes, scale)
+        self.proxy_loss = ProxyNCALoss(n_embeddings, n_classes, scale)
+        self.proxy_weight = proxy_weight
+        self.loss_tracker = tf.keras.metrics.Mean(name='loss')
+        self.loss_tracker_softmax = tf.keras.metrics.Mean(name='softmax_loss')
+        self.loss_tracker_proxy = tf.keras.metrics.Mean(name='proxy_loss')
+
+    def compile(self, optimizer, **kwargs):
+        super(ProxyModel, self).compile(**kwargs)
+        self.optimizer = optimizer
+        self.optimizer_proxy = tfa.optimizers.AdamW(learning_rate=self.proxy_lr, weight_decay=1e-4)
+
+    def train_step(self, data):
+        x, y_true = data
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss1 = self.softmax_loss(y_true, y_pred)
+            loss2 = self.proxy_loss(y_true, y_pred) * self.proxy_weight
+            total_loss = loss1 + loss2
+        trainable_vars = self.trainable_weights
+        trainable_vars += self.softmax_loss.trainable_weights
+        trainable_vars += self.proxy_loss.trainable_weights
+        grads = tape.gradient(total_loss, trainable_vars)
+        spliter = len(trainable_vars) - len(self.proxy_loss.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads[:spliter], trainable_vars[:spliter]))
+        self.optimizer_proxy.apply_gradients(zip(grads[spliter:], trainable_vars[spliter:]))
+        self.loss_tracker.update_state(total_loss)
+        self.loss_tracker_softmax.update_state(loss1)
+        self.loss_tracker_proxy.update_state(loss2)
+        return {'loss': self.loss_tracker.result(),
+            'softmax_loss': self.loss_tracker_softmax.result(),
+            'proxy_loss': self.loss_tracker_proxy.result()
+            }
+
+    @property
+    def metrics(self):
+        return [self.loss_tracker, self.loss_tracker_softmax, self.loss_tracker_proxy]
 
 
 class ArcFaceModel(tf.keras.Model):
