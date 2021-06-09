e8a9079f7904f94845a23ceaba6fb57a290365e3
Im Sunghoon <shi510.kor@gmail.com>
2021-06-09 18:34:15 +0900

    Fix quant level 2

--- convert_tflite/main.py
+++ convert_tflite/main.py
@@ -34,6 +34,7 @@ def convert_tflite_int8(model, calb_data, output_name, quant_level=0):
     if quant_level == 1:
         converter.representative_dataset = representative_dataset_gen
     elif quant_level == 2:
+        converter.representative_dataset = representative_dataset_gen
         converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
         converter.inference_input_type = tf.uint8  # or tf.int8
         converter.inference_output_type = tf.uint8  # or tf.int8
